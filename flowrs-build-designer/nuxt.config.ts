26e8bf1b1752d3f0cb7e2d7251de62b703c76f26
ringmayr <christian.ringmayr1@hm.edu>
2023-10-27 14:28:12 +0200

    cleanup before merge

--- flowrs-build-designer/nuxt.config.ts
+++ flowrs-build-designer/nuxt.config.ts
@@ -23,21 +23,3 @@ export default defineNuxtConfig({
     },
   },
 })
-
-// import Vue from '@vitejs/plugin-vue';
-// export default defineNuxtConfig({
-//     devtools: { enabled: true },
-//     // buildModules: ['@nuxt/typescript-build'],
-//     vite: {
-//         server: {
-//             proxy: {
-//                 '/api': {
-//                     target: 'http://localhost:3000', // Zieladresse Ihrer Rust-API
-//                     changeOrigin: true, // Aktiviere die Änderung des Ursprungs, um CORS-Probleme zu vermeiden
-//                     secure: false,
-//                     ws: true,
-//                 },
-//             },
-//         },
-//     },
-// })
