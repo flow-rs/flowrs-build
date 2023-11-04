c4a85f5238a8fd8348fa50a96e7e215d37fcf6eb
Tobias Peslalz <peslalz.tobias@hm.edu>
2023-11-04 09:23:44 +0100

    :construction: wip flowbuilder

--- flowrs-build-designer/rete/index.ts
+++ flowrs-build-designer/rete/index.ts
@@ -1,13 +1,9 @@
 import { createEditor as createDefaultEditor } from './default'
-import { createEditor as createPerfEditor } from './perf'
-import { createEditor as createCustomEditor } from './customization'
-import { createEditor as create3DEditor } from './3d'
+import { createEditor as createFlowBuilder } from './flowbuilder'
 
 const factory = {
-  'default': createDefaultEditor,
-  'perf': createPerfEditor,
-  'customization': createCustomEditor,
-  '3d': create3DEditor
+  'flowbuilder': createFlowBuilder,
+  'default': createDefaultEditor
 }
 // eslint-disable-next-line no-restricted-globals, no-undef
 const query = typeof location !== 'undefined' && new URLSearchParams(location.search)
