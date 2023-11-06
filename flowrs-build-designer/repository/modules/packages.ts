f940e87ded07f2a1d8799511329a4d02e97f825b
Tobias Peslalz <peslalz.tobias@hm.edu>
2023-11-06 23:57:32 +0100

    :sparkles: create datastruct to read out in and output

--- flowrs-build-designer/repository/modules/packages.ts
+++ flowrs-build-designer/repository/modules/packages.ts
@@ -1,29 +1,64 @@
 
 import FetchFactory from '../factory';
 
-type Crate = {
+export type Crate = {
     name: string;
     version: string;
     crates: Record<string, CrateType>;
 }
 
-type CrateType = {
+export type CrateType = {
     types: Record<string, TypeDefinition>;
     modules: Record<string, ModuleDefinition>;
 }
 
-type TypeDefinition = {
+export type TypeDefinition = {
     inputs: string[] | null;
     outputs: string[] | null;
     type_parameters: string[] | null;
     constructors: Record<string, string>;
 }
 
-type ModuleDefinition = {
+export type ModuleDefinition = {
     types: Record<string, TypeDefinition>;
     modules: Record<string, ModuleDefinition>;
 }
 
+// TODO refactor to a correct place
+export async function createAllPackagesToTypeDefintionMap() : Promise<Map<string, TypeDefinition>> {
+    const {$api} = useNuxtApp();
+    const packages = await $api.packages.getFlowrsPackages();
+    const packageMap: Map<string, TypeDefinition> = new Map<string, TypeDefinition>();
+
+    for (const packagesKey in packages) {
+        let crate = packages[packagesKey];
+        if (!crate) {
+            continue
+        }
+        let crateTypes = crate.crates;
+        for (let crateName in crateTypes) {
+            let crateType = crateTypes[crateName];
+            populatePackageMap(crateType.modules, crateType.types, packageMap, crateName);
+        }
+    }
+
+    return packageMap
+}
+
+function populatePackageMap(moduleDefinition: Record<string, ModuleDefinition>, typeDefinition: Record<string, TypeDefinition>, packageMap: Map<string, TypeDefinition>, packagePath: string) {
+    addTypeDefinitions(typeDefinition, packageMap, packagePath);
+    for (const moduleDefinitionKey in moduleDefinition) {
+        const newPackagePath = packagePath + "::" + moduleDefinitionKey;
+        const newModuleDefinition = moduleDefinition[moduleDefinitionKey];
+        populatePackageMap(newModuleDefinition.modules, newModuleDefinition.types, packageMap, newPackagePath);
+    }
+}
+
+function addTypeDefinitions(typeDefinition: Record<string, TypeDefinition>, packageMap: Map<string, TypeDefinition>, packagePath: string) {
+    for (const typeDefinitionKey in typeDefinition) {
+        packageMap.set(packagePath+ "::"+ typeDefinitionKey, typeDefinition[typeDefinitionKey])
+    }
+}
 
 class PackagesModule extends FetchFactory {
     private RESOURCE = '/packages/';
