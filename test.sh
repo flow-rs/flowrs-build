4afc65fcc49a6a03a217388f33b89c76b1982551
Moritz Maier <contact@moritzphilippmaier.de>
2023-12-20 13:33:20 +0100

    🔨  update test.sh script

--- test.sh
+++ test.sh
@@ -1,41 +1,35 @@
+#!/bin/bash
+
 # Setting Environment Variables
 export RUSTFLAGS="-C instrument-coverage"
+# Uncomment the line below if needed
 # export RUSTDOCFLAGS="-C instrument-coverage -Z unstable-options --persist-doctests target/debug/doctestbins"
 
-# Cleaning, Building, and Testing the Project
 cargo clean
 cargo build
 cargo test
 
-# Merging Coverage Data
-llvm-profdata merge -sparse default_*.profraw -o coverage.profdata
-rm *.profraw
-
 # Run cargo test and parse the output
-cargo test --tests --no-run --message-format=json > cargo_output.txt
+cargo test --tests --no-run --message-format=json > cargo_output.json
 
-# Extracting executable file paths from the output
-grep "\"executable\":\"" cargo_output.txt | grep -v "null" | sed -E 's/.*"executable":"([^"]+)".*/\1/' > executable_files.txt
-
-# Check if executable_files.txt is empty
-if [ ! -s executable_files.txt ]; then
-    echo "No executable files found for coverage."
-    exit 1
-fi
-
-# Prepare arguments for llvm-cov
+# Filter out unwanted files and prepare arguments for llvm-cov
 llvmCovArgs=()
-while IFS= read -r file; do
-    llvmCovArgs+=("-object" "$file")
-done < executable_files.txt
+while read -r file; do
+    if [[ ! $file =~ "dSYM" ]] && [[ ! $file =~ ".pdb" ]] && [[ ! $file =~ ".rmeta" ]]; then
+        llvmCovArgs+=("-object" "$file")
+    fi
+done < <(grep -oP '"filenames":\["\K(.*?)(?="])' cargo_output.json | tr ',' '\n' | sed 's/"//g')
 
 # Generate HTML report using grcov
 grcov . -s . --binary-path ./target/debug/ -t html --branch --ignore-not-existing --ignore "/*" -o ./target/debug/coverage/
 
-# Run llvm-cov report
-llvm-cov export "${llvmCovArgs[@]}" --instr-profile=coverage.profdata --format=lcov -sources ./src/ > coverage.lcov
+# Merge profraw files into a single profdata file
+llvm-profdata merge -sparse default_*.profraw -o coverage.profdata
+rm *.profraw
 
-# Cleanup
-rm executable_files.txt
-rm cargo_output.txt
+# Generate lcov report
+llvm-cov export "${llvmCovArgs[@]}" --instr-profile=coverage.profdata --format=lcov --sources ./src/ > ./target/debug/coverage/lcov.info
+
+# Clean up
 rm *.profdata
+rm cargo_output.json
