6747d3acc930ba2c6a7f49f79cafba57b41e6c2f
hm-maier29 <maier29@hm.edu>
2024-01-05 22:35:14 +0100

    ♻️ ref: refactor out service setup from service_main rs to new module

--- src/bin/service_main.rs
+++ src/bin/service_main.rs
@@ -1,40 +1,11 @@
-use axum::{
-    routing::{get, post},
-    Router,
-};
-use tokio::sync::broadcast;
-
-use dotenv::dotenv;
-
-use tower_http::cors::{Any, CorsLayer};
-
-use axum::routing::delete;
-use clap::Parser;
-use std::fs;
-use std::net::{IpAddr, Ipv4Addr, SocketAddr};
-use std::sync::{Arc, Mutex};
+use anyhow::{Context, Result};
+use std::net::SocketAddr;
 
 use flowrs_build::{
-    api::rest_handlers::{
-        compile_project, create_project, delete_project, get_all_packages, get_all_projects,
-        get_package_by_name, get_process_logs, last_compile_project, run_project, stop_process,
-    },
-    config::load_config,
-    flow_project::FlowProjectManager,
-    package_manager::PackageManager,
+    service::config::load_environment_and_configure, service::server::setup_server,
 };
 
-#[derive(Parser, Debug)]
-#[command(author, version, about, long_about = None)]
-struct Arguments {
-    /// Json config file.
-    #[arg(short, long, default_value_t = f("config.json"))]
-    config_file: String,
-}
-
-fn f(str: &str) -> String {
-    str.to_string()
-}
+use tokio::sync::broadcast;
 
 async fn handle_shutdown_signal(stopper: broadcast::Sender<()>) -> std::io::Result<()> {
     tokio::signal::ctrl_c()
@@ -46,76 +17,19 @@ async fn handle_shutdown_signal(stopper: broadcast::Sender<()>) -> std::io::Resu
 }
 
 #[tokio::main]
-async fn main() {
-    // Read Environment Variables
-    dotenv().ok();
-    let host_ip: String = std::env::var("HOST_IP").expect("HOST_IP must be set correctly");
-    let host_ip_addr: IpAddr = IpAddr::V4(host_ip.parse::<Ipv4Addr>().unwrap());
-    let host_port: String = std::env::var("HOST_PORT").expect("HOST_PORT must be set correctly");
-    let host_port_u16: u16 = host_port.parse::<u16>().unwrap();
-
+async fn main() -> Result<()> {
+    //Spawn async Task for stopping the service
     let (stopper_sender, _) = broadcast::channel::<()>(1);
     let stopper_sender_clone = stopper_sender.clone();
     tokio::spawn(handle_shutdown_signal(stopper_sender_clone));
 
-    let args = Arguments::parse();
+    //loads arguments, environment and config
+    let config = load_environment_and_configure();
+    let addr = SocketAddr::new(config.ip, config.port);
 
-    let config = load_config(&args.config_file.as_str());
-
-    // Setup package manager
-    let package_manager = Arc::new(Mutex::new(PackageManager::new_from_folder(
-        &config.flow_packages_folder,
-    )));
-
-    // Setup project manager.
-    let project_folder = config.flow_project_manager_config.project_folder.clone();
-    let project_manager = Arc::new(Mutex::new(FlowProjectManager::new(
-        config.flow_project_manager_config,
-    )));
-    let res = project_manager.lock().unwrap().load_projects();
-    if let Err(err) = res {
-        println!(
-            "-> Failed to read project folder '{}'. Reason: {}",
-            project_folder, err
-        );
-        println!("-> Create new project folder");
-        if let Err(err) = fs::create_dir(&project_folder) {
-            println!(
-                "-> Failed to create new project folder '{}'. Reason: {}",
-                project_folder, err
-            );
-            return;
-        }
-    }
-
-    let cors = CorsLayer::new()
-        .allow_origin(Any)
-        .allow_methods(Any)
-        .allow_headers(Any);
-    let mut app = Router::new()
-        .route("/packages/:package_name", get(get_package_by_name))
-        .route("/packages/", get(get_all_packages))
-        .with_state(package_manager.clone())
-        //.route("/projects/:project_name", get(get_project_by_name))
-        .route("/projects/", post(create_project))
-        .with_state((project_manager.clone(), package_manager.clone()))
-        .route("/projects/", get(get_all_projects))
-        .route("/projects/:project_name/", delete(delete_project))
-        .route("/projects/:project_name/compile", post(compile_project))
-        .route(
-            "/projects/:project_name/last_compile",
-            get(last_compile_project),
-        )
-        .route("/projects/:project_name/run", post(run_project))
-        .route("/processes/:process_id/stop", post(stop_process))
-        .route("/processes/:process_id/logs", get(get_process_logs))
-        .with_state(project_manager.clone());
-
-    let addr = SocketAddr::new(host_ip_addr, host_port_u16);
-    app = Router::new().nest("/api", app).layer(cors);
-
+    //setup server
+    let app = setup_server(config.clone()).context("Failed to setup server")?;
     println!("-> Listening on {}", addr);
-
     let server = axum::Server::bind(&addr)
         .serve(app.into_make_service())
         .with_graceful_shutdown(async {
@@ -126,4 +40,6 @@ async fn main() {
     let _ = server.await;
 
     println!("-> Service shut down.");
+
+    Ok(())
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,11 @@
-pub mod config;
 pub mod flow_model;
 pub mod flow_project;
 pub mod package;
 pub mod package_manager;
 pub mod api {
     pub mod rest_handlers;
+}
+pub mod service {
+    pub mod config;
+    pub mod server;
 }
