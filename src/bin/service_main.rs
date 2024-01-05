3ba09efdad45bd3a91e571674dbbfe74d8411168
hm-maier29 <maier29@hm.edu>
2024-01-05 16:25:00 +0100

    ♻️ ref: refactor config out of service_main into new module config.rs

--- src/bin/service_main.rs
+++ src/bin/service_main.rs
@@ -11,8 +11,6 @@ use tower_http::cors::{Any, CorsLayer};
 use axum::routing::delete;
 use clap::Parser;
 use std::fs;
-use std::fs::File;
-use std::io::Read;
 use std::net::{IpAddr, Ipv4Addr, SocketAddr};
 use std::sync::{Arc, Mutex};
 
@@ -21,10 +19,10 @@ use flowrs_build::{
         compile_project, create_project, delete_project, get_all_packages, get_all_projects,
         get_package_by_name, get_process_logs, last_compile_project, run_project, stop_process,
     },
-    flow_project::{FlowProjectManager, FlowProjectManagerConfig},
+    config::load_config,
+    flow_project::FlowProjectManager,
     package_manager::PackageManager,
 };
-use serde::{Deserialize, Serialize};
 
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
@@ -36,52 +34,6 @@ struct Arguments {
 
 fn f(str: &str) -> String {
     str.to_string()
-}
-
-#[derive(Debug, Serialize, Deserialize, Clone)]
-struct ServiceConfig {
-    #[serde(default = "flow_project_manager_config_default")]
-    flow_project_manager_config: FlowProjectManagerConfig,
-
-    #[serde(default = "flow_packages_folder_default")]
-    flow_packages_folder: String,
-}
-
-fn flow_project_manager_config_default() -> FlowProjectManagerConfig {
-    FlowProjectManagerConfig::default()
-}
-
-fn flow_packages_folder_default() -> String {
-    "flow-packages".to_string()
-}
-
-impl Default for ServiceConfig {
-    fn default() -> Self {
-        Self {
-            flow_project_manager_config: flow_project_manager_config_default(),
-            flow_packages_folder: flow_packages_folder_default(),
-        }
-    }
-}
-
-fn load_config(config_path: &str) -> ServiceConfig {
-    if std::path::Path::new(config_path).exists() {
-        // Read and deserialize the existing config.json file
-        let mut file =
-            File::open(config_path).expect(&format!("Failed to open {}", config_path).as_str());
-        let mut config_content = String::new();
-        file.read_to_string(&mut config_content)
-            .expect(&format!("Failed to read {}", config_path).as_str());
-        serde_json::from_str(&config_content)
-            .expect(&format!("Failed to deserialize from {}", config_path).as_str())
-    } else {
-        // If the file doesn't exist, create a new FlowProjectManagerConfig with default values.
-        println!(
-            "-> Could not read config file '{}'. Creating default config.",
-            config_path
-        );
-        ServiceConfig::default()
-    }
 }
 
 async fn handle_shutdown_signal(stopper: broadcast::Sender<()>) -> std::io::Result<()> {
