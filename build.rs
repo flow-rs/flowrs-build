08269b9ccdd68e98810519dcefaebefaabe6ed95
mophima <contact@moritzphilippmaier.de>
2024-05-09 14:02:29 +0200

    🚧 Starting to use syn to parse library file structure

--- build.rs
+++ build.rs
@@ -1,9 +1,46 @@
-use std::path::Path;
+use std::{error::Error, fs, path::Path};
 
 use cargo_metadata::{MetadataCommand, Package};
 
-fn is_flow_package_repository(package: Package) -> bool {
+use syn::{File, Item};
+
+// Function to determine if a Cargo Package is a flow-package
+fn is_flow_package(package: Package) -> bool {
     package.keywords.contains(&"flow-package".to_string())
+}
+
+// Function to read flow-nodes from Cargo-Package
+fn extract_flow_nodes(package: Package, package_path: &Path) -> Result<(), Box<dyn Error>> {
+    let src_path = package_path.join("src");
+    //let nodes_path = src_path.join("nodes");
+    let lib_path = src_path.join("lib.rs");
+
+    // Parse lib.rs to get the module structure
+    let file_content = fs::read_to_string(lib_path).expect("Unable to read lib.rs");
+    let syntax_tree = syn::parse_file(&file_content)?;
+    for item in syntax_tree.items {
+        match item {
+            Item::Mod(m) => todo!(),
+            Item::Const(_) => todo!(),
+            Item::Enum(_) => todo!(),
+            Item::ExternCrate(_) => todo!(),
+            Item::Fn(_) => todo!(),
+            Item::ForeignMod(_) => todo!(),
+            Item::Impl(_) => todo!(),
+            Item::Macro(_) => todo!(),
+            Item::Static(_) => todo!(),
+            Item::Struct(_) => todo!(),
+            Item::Trait(_) => todo!(),
+            Item::TraitAlias(_) => todo!(),
+            Item::Type(_) => todo!(),
+            Item::Union(_) => todo!(),
+            Item::Use(_) => todo!(),
+            Item::Verbatim(_) => todo!(),
+            _ => todo!(),
+        }
+    }
+
+    Ok(())
 }
 
 fn main() {
@@ -14,9 +51,10 @@ fn main() {
 
     for package in metadata.packages {
         // Filter for dependencies containing nodes
-        if is_flow_package_repository(package.clone()) {
+        if is_flow_package(package.clone()) {
             let package_path = Path::new(&package.manifest_path).parent().unwrap();
             println!("cargo::warning={}", package_path.to_str().unwrap());
+            extract_flow_nodes(package.clone(), package_path);
         }
     }
 }
