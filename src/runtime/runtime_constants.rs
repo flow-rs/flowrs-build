35656e076a5f6684a6b1557645e979f260c5e54f
mophima <contact@moritzphilippmaier.de>
2025-04-05 13:52:41 +0200

    🏗️ Add R2R_Port constant

--- src/runtime/runtime_constants.rs
+++ src/runtime/runtime_constants.rs
@@ -1,3 +1,4 @@
 // port constants
+pub const R2R_PORT: u16 = 4998; // used to communicate between node runtimes
 pub const SETUP_PORT: u16 = 4999; // used to establish connections between node-runtimes and the orchestrator
-pub const RUNTIME_PORT: u16 = 5000; // used to communicate TO any node-runtime
+pub const RUNTIME_PORT: u16 = 5000; // used to communicate from theorchestrator to any node-runtime
